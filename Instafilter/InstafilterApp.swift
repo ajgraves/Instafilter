13d00b9ee7fb287cc3f989619b08af4ad86e08e3
Aaron Graves <ajgraves@ajgraves.com>
2024-08-08 14:28:46 -0500

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Aaron Graves on 8/8/24.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
