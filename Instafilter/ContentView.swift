42b578e1ceaa66aaa25c8e80fb2d19b0e8614571
Aaron Graves <ajgraves@ajgraves.com>
2024-08-10 16:31:59 -0500

    Completed the challenge day to accomplish:
    1. Try making the Slider and Change Filter buttons disabled if there is no image selected.
    2. Experiment with having more than one slider, to control each of the input keys you care about. For example, you might have one for radius and one for intensity.
    3. Explore the range of available Core Image filters, and add any three of your choosing to the app.
    
    I only added 2/3 filters myself, will circle back to add more at another time.

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -14,6 +14,8 @@ import SwiftUI
 struct ContentView: View {
     @State private var processedImage: Image?
     @State private var filterIntensity = 0.5
+    @State private var filterRadius = 0.5
+    @State private var filterScale = 0.5
     @State private var selectedItem: PhotosPickerItem?
     @State private var showingFilters = false
     
@@ -21,6 +23,7 @@ struct ContentView: View {
     @Environment(\.requestReview) var requestReview
     
     @State private var currentFilter: CIFilter = CIFilter.sepiaTone()
+    @State private var filterButtonText = "Sepia Tone"
     let context = CIContext()
     
     var body: some View {
@@ -42,32 +45,51 @@ struct ContentView: View {
                 
                 Spacer()
                 
-                HStack {
-                    Text("Intensity")
-                    Slider(value: $filterIntensity)
-                        .onChange(of: filterIntensity, applyProcessing)
-                }
-                
-                HStack {
-                    Button("Change Filter", action: changeFilter)
+                Section {
+                    HStack {
+                        Text("Intensity\t")
+                        Slider(value: $filterIntensity)
+                            .onChange(of: filterIntensity, applyProcessing)
+                    }
+                    //.disabled(selectedItem == nil)
                     
-                    Spacer()
+                    HStack {
+                        Text("Radius\t\t")
+                        Slider(value: $filterRadius)
+                            .onChange(of: filterRadius, applyProcessing)
+                    }
+                    //.disabled(selectedItem == nil)
                     
-                    if let processedImage {
-                        ShareLink(item: processedImage, preview: SharePreview("Instafilter image", image: processedImage))
+                    HStack {
+                        Text("Scale\t\t")
+                        Slider(value: $filterScale)
+                            .onChange(of: filterScale, applyProcessing)
+                    }
+                    
+                    HStack {
+                        Button(filterButtonText, action: changeFilter)
+                        
+                        Spacer()
+                        
+                        if let processedImage {
+                            ShareLink(item: processedImage, preview: SharePreview("Instafilter image", image: processedImage))
+                        }
                     }
                 }
+                .disabled(selectedItem == nil)
             }
             .padding([.horizontal, .bottom])
             .navigationTitle("Instafilter")
             .confirmationDialog("Select a filter", isPresented: $showingFilters) {
-                Button("Crystallize") { setFilter(CIFilter.crystallize()) }
-                Button("Edges") { setFilter(CIFilter.edges()) }
-                Button("Gaussian Blur") { setFilter(CIFilter.gaussianBlur()) }
-                Button("Pixellate") { setFilter(CIFilter.pixellate()) }
-                Button("Sepia Tone") { setFilter(CIFilter.sepiaTone()) }
-                Button("Unsharp Mask") { setFilter(CIFilter.unsharpMask()) }
-                Button("Vignette") { setFilter(CIFilter.vignette()) }
+                Button("Crystallize") { setFilter(CIFilter.crystallize(), "Crystallize") }
+                Button("Edges") { setFilter(CIFilter.edges(), "Edges") }
+                Button("Gaussian Blur") { setFilter(CIFilter.gaussianBlur(), "Gaussian Blur") }
+                Button("Pixellate") { setFilter(CIFilter.pixellate(), "Pixellate") }
+                Button("Sepia Tone") { setFilter(CIFilter.sepiaTone(), "Sepia Tone") }
+                Button("Unsharp Mask") { setFilter(CIFilter.unsharpMask(), "Unsharp Mask") }
+                Button("Vignette") { setFilter(CIFilter.vignette(), "Vignette") }
+                Button("Bokeh Blur") { setFilter(CIFilter.bokehBlur(), "Bokeh Blur") }
+                Button("Motion Blur") { setFilter(CIFilter.motionBlur(), "Motion Blur") }
                 Button("Cancel", role: .cancel) {  }
             }
         }
@@ -95,10 +117,10 @@ struct ContentView: View {
             currentFilter.setValue(filterIntensity, forKey: kCIInputIntensityKey)
         }
         if inputKeys.contains(kCIInputRadiusKey) {
-            currentFilter.setValue(filterIntensity * 200, forKey: kCIInputRadiusKey)
+            currentFilter.setValue(filterRadius * 200, forKey: kCIInputRadiusKey)
         }
         if inputKeys.contains(kCIInputScaleKey) {
-            currentFilter.setValue(filterIntensity * 10, forKey: kCIInputScaleKey)
+            currentFilter.setValue(filterScale * 10, forKey: kCIInputScaleKey)
         }
         
         guard let outputImage = currentFilter.outputImage else { return }
@@ -108,8 +130,9 @@ struct ContentView: View {
         processedImage = Image(uiImage: uiImage)
     }
     
-    @MainActor func setFilter(_ filter: CIFilter) {
+    @MainActor func setFilter(_ filter: CIFilter, _ title: String) {
         currentFilter = filter
+        filterButtonText = title
         loadImage()
         
         filterCount += 1
